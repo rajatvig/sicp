4e6ac8c3b1fbd1d8938df0e575c5998bec58c0e0
Rajat Vig <rvig@etsy.com>
2018-10-01 11:03:37 -0700

    start ch2

---
+++ ch2/2.1.rkt
@@ -0,0 +1,44 @@
+#lang sicp
+
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+
+(define (make-rat n d)
+  (cons n d))
+
+(define (denom rat)
+  (cdr rat))
+
+(define (numer rat)
+  (car rat))
+
+(define (add-rat x y)
+  (make-rat (+ (* (denom y) (numer x))
+               (* (denom x) (numer y)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (denom y) (numer x))
+               (* (denom x) (numer y)))
+            (* (denom x) (denom y))))
+
+(define (mult-rat x y)
+  (make-rat (* (numer y) (numer x))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (denom y) (numer x))
+            (* (numer x) (denom y))))
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+       (* (denom x) (numer y))))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x))
+  (newline))
