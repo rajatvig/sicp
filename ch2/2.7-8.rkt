5b8f471e7bb34d1f7ba6d248fecafb4d6e802e34
Rajat Vig <rajat.vig@gmail.com>
2018-10-05 22:25:14 -0700

    2.8 and renames for ch1

--- ch2/2.7-8.rkt
+++ ch2/2.7-8.rkt
@@ -1,4 +1,18 @@
 #lang sicp
+(define (make-interval a b)
+  (cons a b))
+
+(define (upper-bound i)
+  (max (car i) (cdr i)))
+
+(define (lower-bound i)
+  (min (car i) (cdr i)))
+
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x)
+                    (upper-bound y))
+                 (- (upper-bound x)
+                    (lower-bound y))))
 
 (define (add-interval x y)
   (make-interval (+ (lower-bound x)
@@ -23,12 +37,3 @@
                 (make-interval
                  (/ 1.0 (upper-bound y))
                  (/ 1.0 (lower-bound y)))))
-
-(define (make-interval a b)
-  (cons a b))
-
-(define (upper-bound i)
-  (car i))
-
-(define (lower-bound i)
-  (cdr i))
